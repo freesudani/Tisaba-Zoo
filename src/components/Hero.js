94415a4ffefc1bc445352613b50f02d6394e4fde
freesudani <mohamed.dahiya@hotmail.com>
2021-08-07 19:39:09 +0700

    Pages

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section``;
+
+const Container = styled.div``;
+
+const Hero = () => {
+  return (
+    <Section>
+      <Container>
+        <h1>title</h1>
+        <p>Description</p>
+        <button>Learn more</button>
+      </Container>
+    </Section>
+  );
+};
+
+export default Hero;
